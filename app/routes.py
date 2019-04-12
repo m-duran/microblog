e512de6618c48f1040cf5ad3ed4afee4df83b7d3
Miguel Duran <mduran0314@gmail.com>
2019-04-12 01:45:10 -0700

    add post translation link

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, flash, redirect, url_for, request, g
+from flask import render_template, flash, redirect, url_for, request, g, jsonify
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.email import send_password_reset_email
+from app.translate import translate
 from flask_login import current_user, login_user, logout_user, login_required
 from flask_babel import _, get_locale
 from app.models import User, Post
@@ -183,3 +184,11 @@ def reset_password(token):
         flash(_('Your password has been reset.'))
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
+
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({'text': translate(request.form['text'],
+                                      request.form['source_language'],
+                                      request.form['dest_language'])})

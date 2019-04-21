f2cd53c68c1f3a2b2a0c2cfa85f9b83f11ff9ad0
Miguel Duran <mduran0314@gmail.com>
2019-04-20 23:54:13 -0700

    add redis queue and background workers

--- config.py
+++ config.py
@@ -20,3 +20,5 @@ class Config(object):
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     POSTS_PER_PAGE = 25
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
+    REDIS_PORT = os.environ.get('REDIS_PORT') or 6379
